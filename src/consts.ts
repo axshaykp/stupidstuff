b7282100312dd65d5f170a314f57581dfe40d87a
axshaykp <admin@akshaykp.in>
2024-09-27 00:18:45 +0530

    base setup

--- src/consts.ts
+++ src/consts.ts
@@ -1,16 +1,16 @@
 import type { Metadata, Site, Socials } from "@types";
 
 export const SITE: Site = {
-  TITLE: "Astro Micro",
-  DESCRIPTION: "Astro Micro is an accessible and lightweight blog.",
-  EMAIL: "trevortylerlee@gmail.com",
+  TITLE: "Stupid stuff by akshaykp.in",
+  DESCRIPTION: "My helpful tips, ideas, code, etc.",
+  EMAIL: "admin@akshaykp.in",
   NUM_POSTS_ON_HOMEPAGE: 5,
   NUM_PROJECTS_ON_HOMEPAGE: 3,
 };
 
 export const HOME: Metadata = {
   TITLE: "Home",
-  DESCRIPTION: "Astro Micro is an accessible theme for Astro.",
+  DESCRIPTION: "My helpful tips, ideas, code, etc.",
 };
 
 export const BLOG: Metadata = {
@@ -27,14 +27,14 @@ export const PROJECTS: Metadata = {
 export const SOCIALS: Socials = [
   {
     NAME: "X (formerly Twitter)",
-    HREF: "https://twitter.com/boogerbuttcheeks",
+    HREF: "https://twitter.com/axshaykp",
   },
   {
     NAME: "GitHub",
-    HREF: "https://github.com/trevortylerlee",
+    HREF: "https://github.com/axshaykp",
   },
   {
     NAME: "LinkedIn",
-    HREF: "https://www.linkedin.com/in/trevortylerlee",
+    HREF: "https://www.linkedin.com/in/akshay-k-p-2a5001230/",
   },
 ];
